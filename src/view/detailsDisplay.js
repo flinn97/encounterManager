0163581a0e04f09e91d49805671002dc6f98da4c
IsaacB <iwbcheetah@gmail.com>
2024-05-17 18:59:48 -0600

    AudioLink redo

--- src/view/detailsDisplay.js
+++ src/view/detailsDisplay.js
@@ -1,41 +1,52 @@
 
 import { Component } from 'react';
 import { MapComponent } from '../mapTech/mapComponentInterface';
+import { Link } from 'react-router-dom';
 
-export default class DetailsDisplay extends Component{
-    constructor(props){
-      super(props);
-  
-  
-    }
+export default class DetailsDisplay extends Component {
+  constructor(props) {
+    super(props);
 
-    convertStringToLink = (string) => {
-      if (!string) return '';
-    
-      const trimmedString = string.trim();
-      const hasProtocol = /^https?:\/\//i.test(trimmedString);
-    
-      return hasProtocol ? trimmedString : 'https://' + trimmedString;
-    }
-    
-  
-  
-    render(){
-      let app = this.props.app
-      let state = app.state
-      let encounter = state.currentEncounter
 
-      let audioLink = convertStringToLink(encounter?.getJson().audio ? encounter.getJson().audio:encounter.getJson().audioLink);
+  }
 
-      return(<div>
+  convertStringToLink = (string) => {
+    if (!string) return '';
+
+    const trimmedString = string.trim();
+    const hasProtocol = /^https?:\/\//i.test(trimmedString);
+
+    return hasProtocol ? trimmedString : 'https://' + trimmedString;
+  }
+
+
+
+  render() {
+    let app = this.props.app
+    let state = app.state
+    let encounter = state.currentEncounter
+
+    let audioLink = this.convertStringToLink(encounter?.getJson().audio ? encounter.getJson().audio : encounter.getJson().audioLink);
+    let audioPrev = encounter?.getJson().audio ? encounter.getJson().audio : encounter.getJson().audioLink;
+    audioPrev = audioPrev.length > 30 ? audioPrev.substring(0, 30) + "..." : audioPrev;
+
+
+    return (<div>
       <div className='info'>
         {encounter.getJson().description}
       </div>
 
-      <a className='info'>
-        {audioLink}
-      </a>
-      </div>)
-      }
+      {audioPrev &&
+        (<a className='info Audio-Link' href={audioLink} style={{ marginTop: "-18px" }}>
+          {audioPrev}
+        </a>
+        )
+        ||
+        (
+          <div style={{ height:"11px" }}></div>
+        )
+        }
+
+    </div>)
   }
-  
+}
